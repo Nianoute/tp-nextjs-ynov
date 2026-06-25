bb995d0cc47b8cf7835dcf309e18769de6a3576f
Enzo ANGOT <81158245+Nianoute@users.noreply.github.com>
2026-06-25 16:45:33 +0200

    feat : cohérence CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 bg-amber-50">{children}</main>
       </body>
     </html>
   );
