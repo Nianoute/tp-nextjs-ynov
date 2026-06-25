62354d5345ba966516871a2bfc9e4503edc80a11
Enzo ANGOT <81158245+Nianoute@users.noreply.github.com>
2026-06-25 10:57:21 +0200

    fix : build en production

--- app/offres/page.tsx
+++ app/offres/page.tsx
@@ -1,11 +1,10 @@
 import { Offre } from "../../types/offre";
+import Offres from "../../public/offres.json";
 
 export default async function OffresPage() {
-  const offres = await fetch("http://localhost:3000/offres.json");
-  const offresData = await offres.json();
   return (
     <div>
-      {offresData.map((offre: Offre) => (
+      {Offres.map((offre: Offre) => (
         <div key={offre.title}>
           <h1>{offre.title}</h1>
           <p>{offre.description}</p>
